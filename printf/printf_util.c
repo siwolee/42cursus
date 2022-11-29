65a23db1ee1ee9be0632ce3bf6372d217ba9da3b
siwolee <siwolee@student.42seoul.kr>
2022-11-29 11:20:34 +0900

    ..

--- printf/printf_util.c
+++ printf/printf_util.c
@@ -1,31 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.c                                           :+:      :+:    :+:   */
+/*   printf_util.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: siwolee <siwolee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/28 13:04:03 by siwolee           #+#    #+#             */
-/*   Updated: 2022/11/28 13:11:36 by siwolee          ###   ########.fr       */
+/*   Created: 2022/11/28 13:12:43 by siwolee           #+#    #+#             */
+/*   Updated: 2022/11/28 13:13:08 by siwolee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-
-int	ft_printf(const char *buf, ...)
-{
-	size_t i;
-
-	i = 0;
-	while (buf[i])
-	{
-		if (buf[i] == '%')
-			print_type(buf[++i]);
-		else if (buf[i] == '\\')
-			print_escape(buf[++i]);
-		else
-			putchar(buf[i]);
-		i++;
-	}
-}
 
