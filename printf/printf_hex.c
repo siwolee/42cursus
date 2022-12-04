36c093a90a59057123eb10a3fb3a43d4c3065be9
siwolee <siwolee@student.42seoul.kr>
2022-12-04 13:57:09 +0900

    ..

--- printf/printf_hex.c
+++ printf/printf_hex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   printf_hex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siwolee <siwolee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: siwolee <siwolee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 21:12:49 by siwolee           #+#    #+#             */
-/*   Updated: 2022/12/02 22:37:57 by siwolee          ###   ########.fr       */
+/*   Updated: 2022/12/02 23:19:13 by siwolee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,36 @@ int	get_itohex_low(int sign, char buf[], long long arg)
 	i = len - 1;
 	while (i >= 0 && buf[i] != '-')
 	{
-		buf[i] = arg % 10 + '0'];
+		buf[i] = hex[arg % 10];
+		i--;
+		arg /= 10;
+	}
+	return (len);
+}
+
+int	get_itohex_up(int sign, char buf[], long long arg)
+{
+	char		hex[16]="0123456789ABCDEF";
+	int			len;
+	int			i;
+	long long	temp;
+
+	len = 1;
+	temp = arg;
+	if (sign == -1)
+	{
+		len++;
+		buf[0] = '-';
+	}
+	while (temp >= 16)
+	{
+		temp /= 16;
+		len++;
+	}
+	i = len - 1;
+	while (i >= 0 && buf[i] != '-')
+	{
+		buf[i] = hex[arg % 10];
 		i--;
 		arg /= 10;
 	}
@@ -51,7 +80,7 @@ int	print_num_in_hex_low(char *format, int arg)
 	if (arg == 0)
 		return (write(1, "0", 1));
 	if (arg == -2147483647 -1)
-		return (write(1, "-2147483648", 11));
+		return (write(1, "80000000", 8));
 	if (arg < 0)
 	{
 		sign = -1;
@@ -66,28 +95,23 @@ int	print_num_in_hex_low(char *format, int arg)
 
 int	print_num_in_hex_up(char *format, int arg)
 {
-	char 	*hex = "0123456789ABCDEF";
-	char 	res[20];
-	size_t	ptr2;
-	int		size;
-	int		i;
+	char	buf[10];
+	int		sign;
+	int		len;
 
-	i = 0;
-	if (ptr2 == 0)
-		return (write(1, "0x0", 3));
-	write(1, "0x", 2);
-	while (ptr2)
+	sign = 1;
+	if (arg == 0)
+		return (write(1, "0", 1));
+	if (arg == -2147483647 -1)
+		return (write(1, "8000000", 8));
+	if (arg < 0)
 	{
-		res[i] = hex[ptr2 % 16];
-		ptr2 /= 16;
-		i++;
+		sign = -1;
+		arg *= -1;
 	}
-	size = i + 2;
-	while (i > 0)
-	{
-		i--;
-		ft_putchar(res[i]);
-	}
-	format = 0;
-	return (size);
+	len = get_itohex_up(sign, buf, arg);
+	if (format)
+		return (print_format(format, buf, len));
+	else
+		return (write(1, buf, len));
 }
