1de7df43f0eccfd4ab4b88e2351bd52d23fc66e2
siwolee <siwolee@42seoul.student.kr>
2022-12-15 19:04:16 +0900

    ..

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -3,80 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siwolee <siwolee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: siwolee <siwolee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/06 18:53:00 by siwolee           #+#    #+#             */
-/*   Updated: 2022/12/09 01:08:44 by siwolee          ###   ########.fr       */
+/*   Created: 2022/12/15 14:37:40 by siwolee           #+#    #+#             */
+/*   Updated: 2022/12/15 14:38:14 by siwolee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
-
-char	*get_next_line_bonus(int fd)
+char	*_(size_t a, int b)
 {
-	static t_list	*lst;
-	char			*line;
-
-	if (fd < 0 || BUFFER_SIZE < 1)
-		return (0);
-	line = 0;
-	if (!buf)
-		buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (!buf)
-		return (0);
-	line = read_line(&buf, fd);
-	return (line);
+	char *t, c = 0, k = read(b, &c, 1);
+	if (k < 0 || k == 0 && a == 0)
+		return (NULL);
+	if (k == 0 || c == '\n')
+		t = malloc(a + 2);
+	else
+		t = _(a + 1, b);
+	if (t)
+		t[a] = c, t[a + 1] = t[a + 1] * (c != '\n');
+	return (t);
 }
-
-size_t	ft_strlen(const char *s)
+char	*get_next_line(int fd)
 {
-	size_t	size;
-
-	size = 0;
-	while (s && s[size])
-		size++;
-	return (size);
+	return (_(0, fd));
 }
-
-size_t	ft_strncat(char *dst, const char *src, size_t srcsize)
-{
-	size_t	d_len;
-	size_t	s_len;
-	size_t	idx;
-
-	d_len = 0;
-	s_len = 0;
-	idx = -1;
-	if (!src)
-		return (0);
-	while (dst[d_len])
-		d_len++;
-	while (src[s_len])
-		s_len++;
-	while (src[++idx] && idx < srcsize - 1)
-		dst[d_len + idx] = src[idx];
-	dst[d_len + idx] = 0;
-	return (d_len + s_len);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	while (n)
-	{
-		*(char *)s = 0;
-		n--;
-		s++;
-	}
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*ptr;
-
-	ptr = malloc(count * size);
-	if (!ptr)
-		return (0);
-	ft_bzero(ptr, count * size);
-	return (ptr);
-}
-
